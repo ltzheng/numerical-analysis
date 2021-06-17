21e0c911916cfee5ea30a15f4051c7a36ec55c53
ltzheng <zlt0116@mail.ustc.edu.cn>
2021-06-17 03:37:52 +0000

    update numerical analysis

--- analysis/compound_trapezoid.m
+++ analysis/compound_trapezoid.m
@@ -1,6 +1,6 @@
 clear, clc
 format long
-max_range_num = 30;
+max_range_num = 20;
 m_s = linspace(1, max_range_num, max_range_num);
 f = @(x) exp(cos(x));
 % calculated by Wolfram alpha
@@ -23,23 +23,3 @@ ylabel('error')
 result = trapezoid(ind);
 fprintf(['result\t\terror\tm\n']);
 fprintf('%.20f\t%2d\t%d\n', result, min_err, ind);
-
-% clear ,clc;
-% % define function and pre - define real value
-% f = @(x)exp(cos(x));
-% real_val = 7.954926521012845274513219665329394328;
-% for k = 1 : 18
-% x(k) = k;
-% error (k) = abs( real_val - ...
-% my_integrate (f, x(k), -pi , pi ));
-% end
-% semilogy (x, error );
-% % calculate integrate value
-% function ans = my_integrate (f, m, start , end_)
-% ans = 0;
-% h = (end_ - start ) / m;
-% ans = ans + h * (f( start ) + f( end_ )) / 2;
-% for k = 1 : m - 1
-% ans = ans + h * f( start + k * h);
-% end
-% end
